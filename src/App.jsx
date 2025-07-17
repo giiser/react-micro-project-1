e86f64991041dc2bed34b04bd888784811475843
sergii.ignatov <s.ignatov92@gmail.com>
2025-07-17 16:09:05 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Rating from './components/Rating';
+
+const App = () => {
+    return <div>
+        <Rating />
+    </div>
+}
+
+export default App;
---
+++ src/components/Rating.jsx
@@ -0,0 +1,16 @@
+const Rating = () => {
+    const stars = Array.from({length: 5}, (_, i) => i+1);
+
+    return (
+        <div className={'rating-container'}>
+            <h2>Rate your experience</h2>
+            <div className="stars">
+                {stars.map((star) => (
+                    <span key={star} className={'star'}>{'\u2605'}</span>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Rating;
